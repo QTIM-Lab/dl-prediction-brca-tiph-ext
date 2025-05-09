666418669b076e83afc1fc858cdd6d16021669af
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-05-09 12:23:06 +0100

    Updated: scripts/clinical-subtype-analysis/eval_clinical_subtype_analysis.sh, scripts/clinical-subtype-analysis/run_clinical_subtype_analysis.sh

--- scripts/clinical-subtype-analysis/run_clinical_subtype_analysis.sh
+++ scripts/clinical-subtype-analysis/run_clinical_subtype_analysis.sh
@@ -12,16 +12,28 @@ export CUDA_VISIBLE_DEVICES=MIG-752466c4-c584-5e2c-9402-e840a3cf5e6f
 echo 'Started Inference Info about Clinical Subtype TCGA-BRCA Database.'
 
 # List of checkpoint directories for AM_SB (CLAM/ResNet50 Features)
-CHECKPOINT_DIRS=('/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_angiogenesis/2024-11-02_13-45-43'\
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_epithelial_mesenchymal_transition/2024-11-02_15-07-48'\
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_fatty_acid_metabolism/2024-11-02_16-26-16'\
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_oxidative_phosphorylation/2024-11-02_17-46-30'\
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_glycolysis/2024-11-02_18-59-12'\
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/kegg_antigen_processing_and_presentation/2024-11-02_20-13-27'\
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/gobp_t_cell_mediated_cytotoxicity/2024-11-02_21-28-36'\
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/gobp_b_cell_proliferation/2024-11-02_22-42-27'\ 
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/kegg_cell_cycle/2024-11-03_00-01-46'\
- '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/immunosuppression/2024-11-03_01-16-59')
+# CHECKPOINT_DIRS=('/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_angiogenesis/2024-11-02_13-45-43'\
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_epithelial_mesenchymal_transition/2024-11-02_15-07-48'\
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_fatty_acid_metabolism/2024-11-02_16-26-16'\
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_oxidative_phosphorylation/2024-11-02_17-46-30'\
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_glycolysis/2024-11-02_18-59-12'\
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/kegg_antigen_processing_and_presentation/2024-11-02_20-13-27'\
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/gobp_t_cell_mediated_cytotoxicity/2024-11-02_21-28-36'\
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/gobp_b_cell_proliferation/2024-11-02_22-42-27'\ 
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/kegg_cell_cycle/2024-11-03_00-01-46'\
+#  '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/immunosuppression/2024-11-03_01-16-59')
+
+# Dago's Augmented Feature Based Models
+CHECKPOINT_DIRS=('/autofs/space/crater_001/projects/micropath/results/augmented/gobp_b_cell_proliferation/2025-02-10_11-33-37' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/gobp_t_cell_mediated_cytotoxicity/2025-02-10_12-57-08' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/hallmark_angiogenesis/2025-02-10_13-23-05' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/hallmark_epithelial_mesenchymal_transition/2025-02-10_13-58-25' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/hallmark_fatty_acid_metabolism/2025-02-10_14-21-51' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/hallmark_glycolysis/2025-02-10_14-30-02' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/hallmark_oxidative_phosphorylation/2025-02-10_14-34-38' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/immunosuppression/2025-02-10_14-40-02' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/kegg_antigen_processing_and_presentation/2025-02-10_14-48-01' \
+ '/autofs/space/crater_001/projects/micropath/results/augmented/kegg_cell_cycle/2025-02-10_14-52-14')
 
  for checkpoint_dir in "${CHECKPOINT_DIRS[@]}"
 do
