a2891c78e2be84652020a509a3270d2c38ec9084
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-06-06 10:36:03 +0100

    Updated: code/clinical-subtype-analysis/count_samples.py

--- code/clinical-subtype-analysis/count_samples.py
+++ code/clinical-subtype-analysis/count_samples.py
@@ -1,41 +1,41 @@
 # Imports
-import os
 import pandas as pd
-import numpy as np
 
 
 
 # Paths
-paths = [
-    'results/hallmark_angiogenesis/test_inference_info_kf0.csv',
-    'results/kegg_antigen_processing_and_presentation/test_inference_info_kf0.csv',
-    'results/gobp_b_cell_proliferation/test_inference_info_kf0.csv',
-    'results/kegg_cell_cycle/test_inference_info_kf0.csv',
-    'results/hallmark_epithelial_mesenchymal_transition/test_inference_info_kf0.csv',
-    'results/hallmark_fatty_acid_metabolism/test_inference_info_kf0.csv',
-    'results/hallmark_glycolysis/test_inference_info_kf0.csv',
-    'results/immunosuppression/test_inference_info_kf0.csv',
-    'results/hallmark_oxidative_phosphorylation/test_inference_info_kf0.csv',
-    'results/gobp_t_cell_mediated_cytotoxicity/test_inference_info_kf0.csv'
-]
+splits = ["train", "val", "test"]
+for split in splits:
+    paths = [
+        f'results/hallmark_angiogenesis/{split}_inference_info_kf0.csv',
+        f'results/kegg_antigen_processing_and_presentation/{split}_inference_info_kf0.csv',
+        f'results/gobp_b_cell_proliferation/{split}_inference_info_kf0.csv',
+        f'results/kegg_cell_cycle/{split}_inference_info_kf0.csv',
+        f'results/hallmark_epithelial_mesenchymal_transition/{split}_inference_info_kf0.csv',
+        f'results/hallmark_fatty_acid_metabolism/{split}_inference_info_kf0.csv',
+        f'results/hallmark_glycolysis/{split}_inference_info_kf0.csv',
+        f'results/immunosuppression/{split}_inference_info_kf0.csv',
+        f'results/hallmark_oxidative_phosphorylation/{split}_inference_info_kf0.csv',
+        f'results/gobp_t_cell_mediated_cytotoxicity/{split}_inference_info_kf0.csv'
+    ]
 
 
-# Go through paths
-for path_ in paths:
+    # Go through paths
+    for path_ in paths:
 
-    # Open file
-    inf_info = pd.read_csv(path_)
-    print('HER2-/HR-', len(inf_info.copy()[inf_info['c_subtype'] == 'HER2-/HR-']))
-    print('HER2-/HR+', len(inf_info.copy()[inf_info['c_subtype'] == 'HER2-/HR+']))
-    print('HER2+/HR-', len(inf_info.copy()[inf_info['c_subtype'] == 'HER2+/HR-']))
-    print('HER2+/HR+', len(inf_info.copy()[inf_info['c_subtype'] == 'HER2+/HR+']))
-    
-    # c_subtype = inf_info.copy()['c_subtype']
-    # print(c_subtype)
-    # print(c_subtype[c_subtype['c_subtype'] == 'HER2-/HR+'])
+        # Open file
+        inf_info = pd.read_csv(path_)
+        print('HER2-/HR-', len(inf_info.copy()[inf_info['c_subtype'] == 'HER2-/HR-']))
+        print('HER2-/HR+', len(inf_info.copy()[inf_info['c_subtype'] == 'HER2-/HR+']))
+        print('HER2+/HR-', len(inf_info.copy()[inf_info['c_subtype'] == 'HER2+/HR-']))
+        print('HER2+/HR+', len(inf_info.copy()[inf_info['c_subtype'] == 'HER2+/HR+']))
+        
+        # c_subtype = inf_info.copy()['c_subtype']
+        # print(c_subtype)
+        # print(c_subtype[c_subtype['c_subtype'] == 'HER2-/HR+'])
 
-    # print(c_subtype.unique())
-    # ['HER2-/HR+']
-    # ['HER2-/HR-'] 
-    # ['HER2+/HR-'] 
-    # ['HER2+/HR+']
+        # print(c_subtype.unique())
+        # ['HER2-/HR+']
+        # ['HER2-/HR-'] 
+        # ['HER2+/HR-'] 
+        # ['HER2+/HR+']
