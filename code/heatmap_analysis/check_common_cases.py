510b43579ff9e8fecfeb1bbe759faec77135bfa6
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-07-04 12:21:52 +0100

    code/heatmap_analysis/check_common_cases.py

--- code/heatmap_analysis/check_common_cases.py
+++ code/heatmap_analysis/check_common_cases.py
@@ -51,12 +51,73 @@ img_count_values = [k for k in image_counts_inv.keys()]
 # print(img_count_values)
 
 for cnt in img_count_values:
-    cnt_dict = {cnt:list()}
+    cnt_dict = {
+        'wsi':list(),
+        'has_angiogenesis':list(),
+        'angiogenesis_path':list(),
+        'has_cell_cycle':list(),
+        'cell_cycle_path':list(),
+        'has_epithelial_mesenchymal_transition':list(),
+        'epithelial_mesenchymal_transition_path':list(),
+        'has_immunosuppression':list(),
+        'immunosuppression_path':list(),
+        'has_t_cell_mediated_cytotoxicity':list(),
+        't_cell_mediated_cytotoxicity_path':list(),
+    }
     for i_cnt, i_name in image_counts_inv.items():
         if i_cnt == cnt:
             # print(i_name)
             for fpath in i_name:
                 cnt_dict[cnt].append(fpath)
+
+                # Check where we have these heatmaps
+                # Angiogenesis
+                a = [c for c in os.listdir(os.path.join(paths_to_assess[1], fpath)) if not c.startswith('.')]
+                if len(a) >= 1:
+                    cnt_dict['has_angiogenesis'].append(1)
+                    cnt_dict['angiogenesis_path'].append(os.path.join(paths_to_assess[1], fpath))
+                else:
+                    cnt_dict['has_angiogenesis'].append(0)
+                    cnt_dict['angiogenesis_path'].append('')
+                
+                # Cell Cyle
+                cc = [c for c in os.listdir(os.path.join(paths_to_assess[3], fpath)) if not c.startswith('.')]
+                if len(cc) >= 1:
+                    cnt_dict['has_cell_cycle'].append(1)
+                    cnt_dict['cell_cycle_path'].append(os.path.join(paths_to_assess[3], fpath))
+                else:
+                    cnt_dict['has_cell_cycle'].append(0)
+                    cnt_dict['cell_cycle_path'].append('')
+                
+                # Epithelial Mesenchymal Transition
+                emt = [c for c in os.listdir(os.path.join(paths_to_assess[2], fpath)) if not c.startswith('.')]
+                if len(emt) >= 1:
+                    cnt_dict['has_epithelial_mesenchymal_transition'].append(1)
+                    cnt_dict['epithelial_mesenchymal_transition_path'].append(os.path.join(paths_to_assess[2], fpath))
+                else:
+                    cnt_dict['has_epithelial_mesenchymal_transition'].append(0)
+                    cnt_dict['epithelial_mesenchymal_transition_path'].append('')
+                
+                # Immunosuppression
+                imm = [c for c in os.listdir(os.path.join(paths_to_assess[4], fpath)) if not c.startswith('.')]
+                if len(imm) >= 1:
+                    cnt_dict['has_immunosuppression'].append(1)
+                    cnt_dict['immunosuppression_path'].append(os.path.join(paths_to_assess[4], fpath))
+                else:
+                    cnt_dict['has_immunosuppression'].append(0)
+                    cnt_dict['immunosuppression_path'].append('')
+                
+                # T Cell Mediated Cytotoxicity
+                tcm = [c for c in os.listdir(os.path.join(paths_to_assess[0], fpath)) if not c.startswith('.')]
+                if len(tcm) >= 1:
+                    cnt_dict['has_t_cell_mediated_cytotoxicity'].append(1)
+                    cnt_dict['t_cell_mediated_cytotoxicity_path'].append(os.path.join(paths_to_assess[0], fpath))
+                else:
+                    cnt_dict['has_t_cell_mediated_cytotoxicity'].append(0)
+                    cnt_dict['t_cell_mediated_cytotoxicity_path'].append('')
+
+
+
     cnt_dict_df = pd.DataFrame.from_dict(cnt_dict)
     # print(cnt_dict_df)
     cnt_dict_df.to_csv(f"common_results_idx{cnt}.csv", index=False)
