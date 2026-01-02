91c69f07d8ae9a676e525f58021c2cc95d8371eb
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2026-01-02 19:40:03 +0000

    Added scripts to generate the plots for clinical subtype analysis.

--- code/clinical-subtype-analysis/create_results_plots_v2.py
+++ code/clinical-subtype-analysis/create_results_plots_v2.py
@@ -588,6 +588,6 @@ if __name__ == "__main__":
             metrics=metrics,
             task=task,
             title=task,
-            ylim=(violin_df[violin_df['task'] == task]['value'].min()-0.20, 1.20),
+            ylim=(violin_df[violin_df['task'] == task]['value'].min()-0.50, 1.20),
             output_path=output_path
         )
